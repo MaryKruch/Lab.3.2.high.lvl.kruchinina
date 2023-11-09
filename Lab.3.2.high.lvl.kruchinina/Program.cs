4294b946279c7275b67d923c5644a24c1ce49285
1 <1@DESKTOP-ACMQRLH>
2023-11-09 23:25:23 +0200

    Добавьте файлы проекта.

---
+++ Lab.3.2.high.lvl.kruchinina/Program.cs
@@ -0,0 +1,21 @@
+﻿//20 var
+
+Console.Write("Введите натуральное число n: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+
+int count = 0;
+int k = n;
+int temp = n;
+int sum = 0;
+
+while (count != m)
+{
+    int j = temp % 10;
+    sum += j;
+    temp /= 10;
+    count++;
+}
+
+Console.WriteLine($"Сумма {m} последних цифр числа {n} равна: {sum}");
